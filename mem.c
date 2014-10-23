e6181a2cf112dffdcb08f8321a8f1214e5a1fbae
cschwalm <schwalm@wisc.edu>
2014-10-23 12:01:44 -0500

    Initial mem_alloc work

--- mem.c
+++ mem.c
@@ -1,13 +1,69 @@
-
+#include "stddef.h"
+#include "stdlib.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
 #include "mem.h"
+#include "unistd.h"
+#include "sys/mman.h"
 
+//Free Node
 typedef struct __node_t {
 	int size;
 	struct __node_t *next; 
-} node_t
+	struct __node_t *prev;
+} node_t;
 
-struct __node_t *head = NULL;
+//Allocated Memory header
+typedef struct __header_t {
+	int size;
+	int magic;
+} header_t;
+
+node_t* head = NULL;
 int freeSpace = 0;
+
+//
+// Modifies the free list so that a new mem node can be added.
+// This removes a node from the free list.
+//
+void
+removeFreeNode(node_t* freeNode)
+{
+	
+	node_t* prevNode, nextNode;
+
+	prevNode = freeNode->prev;
+	nextNode = freeNode->next;
+
+	if (prevNode != NULL)
+	{
+		prevNode->next = nextNode;
+	}
+	else
+	{
+		/* Update the global head pointer. This is the start. */
+		head = nextNode;
+	}
+
+	if (nextNode != NULL)
+	{
+		nextNode->prev = prevNode;
+	}
+
+	freeNode->next = NULL;
+	freeNode->prev = NULL;
+}
+
+void
+split(node_t* memBlock, node_t* newNode, int size)
+{
+	newNode = NULL;
+	if (memBlock->size <= size + sizeof(node_t))
+		return;
+
+	newNode = (node_t*) (memBlock + sizeof(node_t) + size);
+}
 
 int
 Mem_Init(int sizeOfRegion)
@@ -35,6 +91,7 @@ Mem_Init(int sizeOfRegion)
 	
 	head->size = sizeOfRegion - sizeof(node_t);
 	head->next = NULL;
+	head->prev = NULL;
 
 	//close the device (don't worry, mapping should be unaffected)
 	close(fd);
@@ -44,6 +101,28 @@ Mem_Init(int sizeOfRegion)
 void
 *Mem_Alloc(int size)
 {
+	node_t tmp;
+	struct __header_t header;
+
+  int byteAlligned = size / 8;
+  if (size % 8 != 0)
+    byteAlligned++;
+
+  byteAlligned = byteAlligned * 8;
+
+
+	if (byteAlligned + sizeof(header_t) > freeSpace)
+		return NULL;
+
+	tmp = head;
+	do 
+	{
+		if(tmp->size >= (byteAlligned + sizeof(header_t)))
+		{
+			header = 
+		}	
+	} while(tmp->next != NULL);
+		
 	return NULL;
 }
 
